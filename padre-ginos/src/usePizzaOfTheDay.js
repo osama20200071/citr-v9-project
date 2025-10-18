2fb0c8f5fc817ea0b49a0287124650ea8833bc1c
Osama <osamakaram118@gmail.com>
2025-10-18 19:30:34 +0300

    build first custom hook

---
+++ padre-ginos/src/usePizzaOfTheDay.js
@@ -0,0 +1,19 @@
+import { useDebugValue, useEffect, useState } from "react";
+
+export const usePizzaOfTheDay = () => {
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+  useDebugValue(pizzaOfTheDay ? `${pizzaOfTheDay.name}` : "Loading...");
+
+  useEffect(() => {
+    async function fetchPizza() {
+      await new Promise((res) => setTimeout(res, 5000));
+      const res = await fetch("/api/pizza-of-the-day");
+      const data = await res.json();
+      setPizzaOfTheDay(data);
+    }
+
+    fetchPizza();
+  }, []);
+
+  return pizzaOfTheDay;
+};
