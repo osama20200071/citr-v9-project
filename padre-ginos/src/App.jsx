c7465f83b95599123853056ab358884d99b49887
Osama <osamakaram118@gmail.com>
2025-10-18 21:17:58 +0300

    building the cart context

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,14 +1,20 @@
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
 import PizzaOfTheDay from "./PizzaOfTheDay";
+import Header from "./Header";
+import { CartContext } from "./contexts";
+import { useState } from "react";
 
 const App = () => {
+  const cartHook = useState([]);
   return (
-    <div>
-      <h1 className="logo">Padre Gino's Pizza</h1>
-      <Order />
-      <PizzaOfTheDay />
-    </div>
+    <CartContext.Provider value={cartHook}>
+      <div>
+        <Header />
+        <Order />
+        <PizzaOfTheDay />
+      </div>
+    </CartContext.Provider>
   );
 };
 
---
+++ padre-ginos/src/contexts.jsx
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+// as it will provide the hook
+export const CartContext = createContext([[], () => {}]);
