a5a874b7db2b35e1ddbd06e954a40955a157b830
Osama <osamakaram118@gmail.com>
2025-10-26 22:02:21 +0300

    setup Vitest for testing

--- padre-ginos/vite.config.js
+++ padre-ginos/vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  test: {
+    environment: "happy-dom",
+  },
   server: {
     proxy: {
       "/api": {
