91accb72d1126673bcc1de6590c083e48b7534cc
Osama <osamakaram118@gmail.com>
2025-10-28 18:52:35 +0300

    TDD for Pizza component

--- padre-ginos/src/__tests__/Pizza.test.jsx
+++ padre-ginos/src/__tests__/Pizza.test.jsx
@@ -1,6 +1,10 @@
-import { render } from "@testing-library/react";
-import { test, expect } from "vitest";
+import { cleanup, render } from "@testing-library/react";
+import { test, expect, afterEach } from "vitest";
 import Pizza from "../Pizza";
+
+// to cleanup the testing lib after each test
+// To prevent state contamination between tests “which could affect other tests”
+afterEach(cleanup);
 
 test("alt text renders on image", async () => {
   const name = "My Pizza";
@@ -21,4 +25,19 @@ test("alt text renders on image", async () => {
   const img = screen.getByRole("img");
   expect(img.alt).toBe(name);
   expect(img.src).toBe(src);
+});
+
+// here is an example of TTD : we are writing our test which will fail as our Pizza component
+// don't have default value for image till now.
+// so we write our tests first and then make it work.
+test("img should have default value", async () => {
+  const screen = render(
+    <Pizza
+      name="MyFav Pizza"
+      description="Description for my favorite pizza"
+    />,
+  );
+
+  const img = screen.getByRole("img");
+  expect(img.src).not.toBe("");
 });
