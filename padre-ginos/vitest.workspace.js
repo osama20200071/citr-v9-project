5892adc2f68c3813ba8825ffc7e8a9912bbe53e5
Osama <osamakaram118@gmail.com>
2025-10-31 20:38:55 +0200

    setup vitest config for our two envs

---
+++ padre-ginos/vitest.workspace.js
@@ -0,0 +1,28 @@
+import { defineWorkspace } from "vitest/config.js";
+
+// we are defining two evn:
+export default defineWorkspace([
+  //  happy-dom env for our old tests
+  {
+    extends: "./vite.config.js",
+    test: {
+      name: "happy-dom", // to give those tests a label
+      environment: "happy-dom",
+      include: ["**/*.node.test.{js,jsx}"],
+    },
+  },
+  // and playwright for our new browser-based tests
+  {
+    extends: "./vite.config.js",
+    test: {
+      name: "browser",
+      setupFiles: ["vitest-browser-react"], // this runs before our tests
+      include: ["**/*.browser.test.{js,jsx}"],
+      browser: {
+        name: "chromium", // the browser to test in "chromium or firefox or webkit(safari) here too"
+        provider: "playwright",
+        enabled: true,
+      },
+    },
+  },
+]);
